dc730a2faa893e82b15a3f95ba499350567c5e07
tiny-tinker <flashingtinker@gmail.com>
2018-05-18 04:18:59 -0400

    install all dependancies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+import App from './scenes/app';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const target = document.querySelector('#root');
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  target
+);
 registerServiceWorker();
